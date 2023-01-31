b6fa55379f5083921c1846fd2ec2307b215aa189
Lucas Nørgård <lucasnrgaard@gmail.com>
2023-01-31 11:51:50 +0100

    chore: started on better tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,6 +1,6 @@
 import path from "node:path";
 
-import { describe, expect, it } from "vitest";
+import { describe, expect, test } from "vitest";
 
 import {
   findTSConfig,
@@ -11,58 +11,56 @@ import {
   resolveTSConfigSync
 } from "../src";
 
-describe("tsconf-utils", () => {
-  describe("parse", async () => {
-    const fwd = path.join(__dirname, "fixtures", "test-2");
-    const configPath = await findTSConfig(fwd, "tsconfig.json");
+describe("parse", async () => {
+  const fwd = path.join(__dirname, "fixtures", "test-2");
+  const configPath = await findTSConfig(fwd, "tsconfig.json");
 
-    it("parse - async", async () => {
-      const config = await parseTSConfig(configPath!);
-      expect(config).not.toBe(null);
-    });
-
-    it("parse - sync", () => {
-      const config = parseTSConfigSync(configPath!);
-      expect(config).not.toBe(null);
-    });
+  test("parse - async", async () => {
+    const config = await parseTSConfig(configPath!);
+    expect(config).not.toBe(null);
   });
 
-  describe("resolve", () => {
-    const fwd = path.join(__dirname, "fixtures", "test-2");
-    it("resolve - async", async () => {
-      const config = await resolveTSConfig(fwd, "tsconfig.json");
-      expect(config).not.toBe(null);
-    });
+  test("parse - sync", () => {
+    const config = parseTSConfigSync(configPath!);
+    expect(config).not.toBe(null);
+  });
+});
 
-    it("resolve - sync", () => {
-      const config = resolveTSConfigSync(fwd, "tsconfig.json");
-      expect(config).not.toBe(null);
-    });
+describe("resolve", () => {
+  const fwd = path.join(__dirname, "fixtures", "test-2");
+  test("resolve - async", async () => {
+    const config = await resolveTSConfig(fwd, "tsconfig.json");
+    expect(config).not.toBe(null);
   });
 
-  describe("resolve 3", () => {
-    const fwd = path.join(__dirname, "fixtures", "test-3");
-    it("resolve - async", async () => {
-      const config = await resolveTSConfig(fwd, "tsconfig.json");
-      expect(config).not.toBe(null);
-    });
+  test("resolve - sync", () => {
+    const config = resolveTSConfigSync(fwd, "tsconfig.json");
+    expect(config).not.toBe(null);
+  });
+});
 
-    it("resolve - sync", () => {
-      const config = resolveTSConfigSync(fwd, "tsconfig.json");
-      expect(config).not.toBe(null);
-    });
+describe("resolve 3", () => {
+  const fwd = path.join(__dirname, "fixtures", "test-3");
+  test("resolve - async", async () => {
+    const config = await resolveTSConfig(fwd, "tsconfig.json");
+    expect(config).not.toBe(null);
   });
 
-  describe("find", () => {
-    const fwd = path.join(__dirname, "fixtures", "test-1");
-    it("find - async", async () => {
-      const config = await findTSConfig(fwd, "tsconfig.json");
-      expect(config).not.toBe(null);
-    });
+  test("resolve - sync", () => {
+    const config = resolveTSConfigSync(fwd, "tsconfig.json");
+    expect(config).not.toBe(null);
+  });
+});
 
-    it("find - sync", () => {
-      const config = findTSConfigSync(fwd, "tsconfig.json");
-      expect(config).not.toBe(null);
-    });
+describe("find", () => {
+  const fwd = path.join(__dirname, "fixtures", "test-1");
+  test("find - async", async () => {
+    const config = await findTSConfig(fwd, "tsconfig.json");
+    expect(config).not.toBe(null);
+  });
+
+  test("find - sync", () => {
+    const config = findTSConfigSync(fwd, "tsconfig.json");
+    expect(config).not.toBe(null);
   });
 });
